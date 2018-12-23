78055b9cd0cc36b2c70c8e88dd46e738ecf9e7bc
samvid zare <samvid718@gmail.com>
2018-12-23 22:03:27 +0530

    added required training data for ml

--- classification_algorithm.py
+++ classification_algorithm.py
@@ -1,22 +1,22 @@
 def get_comment_type_positive_or_negative():
-   with open( "/Users/LENOVO/Downloads/sentiment labelled sentences/imdb_labelled.txt", "r") as text_file:
-    lines = text_file.read().split('\n')
-   with open( "/Users/LENOVO/Downloads/sentiment labelled sentences/yelp_labelled.txt", "r") as text_file:
-    lines += text_file.read().split('\n')
-   with open( "/Users/LENOVO/Downloads/sentiment labelled sentences/amazon_cells_labelled.txt", "r") as text_file:
-    lines += text_file.read().split('\n')
-   lines = [line.split("\t") for line in lines if len(line.split("\t"))==2 and line.split("\t")[1]<>'']
-   train_documents = [line[0] for line in lines]
-   train_labels = [int(line[1]) for line in lines]
-   from sklearn.feature_extraction.text import CountVectorizer
-   count_vectorizer = CountVectorizer(binary='true')
-   train_documents = count_vectorizer.fit_transform(train_documents)
-   from sklearn.naive_bayes import BernoulliNB
-   classifier = BernoulliNB().fit(train_documents, train_labels)
-   print(classifier.predict(count_vectorizer.transform(["i liked the movie"])))
+    comment = raw_input("Enter the review comment givenn by customer  : ")
+    with open("imdb_labelled.txt", "r") as text_file:
+        lines = text_file.read().split('\n')
+    with open("yelp_labelled.txt", "r") as text_file:
+        lines += text_file.read().split('\n')
+    with open("amazon_cells_labelled.txt", "r") as text_file:
+        lines += text_file.read().split('\n')
+    lines = [line.split("\t") for line in lines if len(line.split("\t")) == 2 and line.split("\t")[1] <> '']
+    train_documents = [line[0] for line in lines]
+    train_labels = [int(line[1]) for line in lines]
+    from sklearn.feature_extraction.text import CountVectorizer
+    count_vectorizer = CountVectorizer(binary='true')
+    train_documents = count_vectorizer.fit_transform(train_documents)
+    from sklearn.naive_bayes import BernoulliNB
+    classifier = BernoulliNB().fit(train_documents, train_labels)
+    if(classifier.predict(count_vectorizer.transform([comment]))==1):
+        print "customer is satisfied"
+    else:
+        print "customer is not satisfied"
 
 get_comment_type_positive_or_negative()
-
-
-
-
